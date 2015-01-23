6a4bcd9432911586efe661be5f188c001ccf2555
Yann Lombard <contact@yannlombard.com>
2015-01-24 00:03:04 +0100

    adjust markers opacity on mouse over

--- client/app/map/map.controller.js
+++ client/app/map/map.controller.js
@@ -2,6 +2,8 @@
 
 angular.module('angularSeoTestFrontApp').controller('MapCtrl', function(rooms) {
 
+    var vm = this;
+
     this.map = {
         center: {
             latitude : 45.7579555,
@@ -15,5 +17,39 @@ angular.module('angularSeoTestFrontApp').controller('MapCtrl', function(rooms) {
     };
 
     this.rooms = rooms;
+
+    var mouseEnter = function(room) {
+        angular.forEach(vm.rooms, function(room) {
+            room.options = room.options || {};
+            room.options.opacity = 0.4;
+        });
+
+        room.hover = true;
+        room.options = room.options || {};
+        room.options.opacity = 1;
+        vm.listHover = true;
+    };
+
+    var mouseLeave = function(room) {
+        angular.forEach(vm.rooms, function(room) {
+            room.options = room.options || {};
+            room.options.opacity = 1;
+        });
+
+        room.hover = false;
+        vm.listHover = false;
+    };
+
+    this.mouseEnterRoom = mouseEnter;
+    this.mouseLeaveRoom = mouseLeave;
+
+    this.markersEvents = {
+        mouseover: function(gMarker, eventName, model) {
+            mouseEnter(model);
+        },
+        mouseout: function(gMarker, eventName, model) {
+            mouseLeave(model);
+        }
+    };
 
 });
